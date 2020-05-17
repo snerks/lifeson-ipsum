6ae1f0149814c31bf9972caea748231957f67174
snerks <martin.github@outlook.com>
2020-05-17 18:41:54 +0100

    Add getSentence

--- mod.ts
+++ mod.ts
@@ -1,11 +1,37 @@
-export const getParagraphs = (count: number): string => {
+export const getSentence = (wordCount?: number): string => {
+  if (wordCount !== undefined && wordCount < 1) {
+    throw new Error("wordCount must be greater than 0");
+  }
+  const count = wordCount || 20;
   const words: string[] = [];
 
+  const commaIndices = [1, 4, 5, 7, 9, 11, 14, 15, 17];
+
   for (let index = 0; index < count; index++) {
-    words.push("blah");
+    let word = index === 0 ? "Blah" : "blah";
+
+    if (commaIndices.indexOf(index) > -1) {
+      word += ",";
+    }
+    words.push(word);
   }
 
-  return words.join(" ");
+  return `${words.join(" ")}.`;
 };
 
-// console.log(getParagraphs(4));
+export const getParagraph = (sentenceCount?: number): string => {
+  if (sentenceCount !== undefined && sentenceCount < 1) {
+    throw new Error("sentenceCount must be greater than 0");
+  }
+
+  const count = sentenceCount || 4;
+  const sentences: string[] = [];
+
+  for (let index = 0; index < count; index++) {
+    sentences.push(getSentence());
+  }
+
+  return sentences.join("\n");
+};
+
+console.log(getParagraph());
